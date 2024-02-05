700881de60e39e623a9f3c668856b02baef34f04
g.umegaki <g.umegaki@buysell-technologies.com>
2024-02-05 18:56:34 +0900

    add role

--- terraform/modules/service_account/main.tf
+++ terraform/modules/service_account/main.tf
@@ -18,6 +18,11 @@ resource "google_project_iam_member" "ga_cloudsql_client" {
   role    = "roles/cloudsql.client"
   member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
+resource "google_project_iam_member" "ga_cloudrun_developer" {
+  project = var.project_id
+  role    = "roles/run.developer"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
 
 resource "google_service_account" "cloudrun" {
   account_id   = "cloudrun-service-account"
