636b714d13ab939efb3c6544fcc3148e409854c7
梅垣玄 <genpsp10@gmail.com>
2024-03-03 01:48:48 +0900

    seed追加

--- src/migration/seed/local/20240201164558_init.up.sql
+++ src/migration/seed/local/20240201164558_init.up.sql
@@ -1,1 +1,3 @@
-INSERT INTO images (title, url) VALUES ("画像", "")
+INSERT INTO users (id, firebase_uid) VALUES (1, "Ps0iFZUEK4djyYdIRIEUgt95dAG2");
+INSERT INTO images(user_id, title, thumbnail_url, detail_url, purchase_url, approval_status)
+VALUES (1, "画像", "https://image.com/test.png", "https://image.com/test.png", "https://image.com/test.png", "PENDING");
