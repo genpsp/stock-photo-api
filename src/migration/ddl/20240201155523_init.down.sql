81ba356fda6802ec3d7189cd4eec56d01e46e14f
梅垣玄 <genpsp10@gmail.com>
2024-03-03 01:48:41 +0900

    テーブル追加

--- src/migration/ddl/20240201155523_init.down.sql
+++ src/migration/ddl/20240201155523_init.down.sql
@@ -1,1 +1,10 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE users;
 DROP TABLE images;
+DROP TABLE tags;
+DROP TABLE image_tags;
+DROP TABLE carts;
+DROP TABLE cart_items;
+DROP TABLE favorites;
+DROP TABLE points;
+SET FOREIGN_KEY_CHECKS = 1;
--- src/migration/ddl/20240201155523_init.up.sql
+++ src/migration/ddl/20240201155523_init.up.sql
@@ -1,8 +1,84 @@
+CREATE TABLE users (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    firebase_uid VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    deleted_at DATETIME DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE images (
-  id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  title      VARCHAR(100) NOT NULL,
-  url        VARCHAR(255) NOT NULL,
-  created_at DATETIME  NOT NULL DEFAULT now(),
-  updated_at DATETIME  NOT NULL DEFAULT now(),
-  PRIMARY KEY (id)
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT UNSIGNED NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    thumbnail_url VARCHAR(255) NOT NULL,
+    detail_url VARCHAR(255) NOT NULL,
+    purchase_url VARCHAR(255) NOT NULL,
+    approval_status VARCHAR(50) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    deleted_at DATETIME DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE tags (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE image_tags (
+    image_id BIGINT UNSIGNED NOT NULL,
+    tag_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY (image_id, tag_id),
+    FOREIGN KEY (image_id) REFERENCES images(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
+
+CREATE TABLE carts (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE cart_items (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cart_id BIGINT UNSIGNED NOT NULL,
+    image_id BIGINT UNSIGNED NOT NULL,
+    quantity INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY (id),
+    FOREIGN KEY (cart_id) REFERENCES carts(id),
+    FOREIGN KEY (image_id) REFERENCES images(id)
+);
+
+CREATE TABLE favorites (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT UNSIGNED NOT NULL,
+    image_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    updated_at DATETIME NOT NULL DEFAULT now(),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (image_id) REFERENCES images(id)
+);
+
+CREATE TABLE points (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT UNSIGNED NOT NULL,
+    points INT NOT NULL,
+    point_type VARCHAR(30) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now(),
+    expiration_date DATETIME DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
